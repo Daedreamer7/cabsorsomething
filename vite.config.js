c6dbaf3d317f06e544cb9a119021b1d09cc7816d
angeliadenise <angeliadenise@gmail.com>
2025-02-13 13:51:51 -0600

    Update domain to conceptsonconcepts.com

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-    host: 'cabsorsomething.concepts.concepts.com'
+    host: 'cabsorsomething.conceptsonconcepts.com'
   },
   build: {
     outDir: 'dist',
